0d51214ad3807795ce9e27b1f8f161e43e0d4061
avasoares <avso4695@colorado.edu>
2024-11-12 10:29:22 -0700

    updated ica 12

--- js/ica12.js
+++ js/ica12.js
@@ -1,6 +1,6 @@
 let ourObject = {
-    "name": "ava"
-    "profession": "Strategic Communcation Student"
+    "name": "ava",
+    "profession": "Strategic Communcation Student",
     "age": 21,
     "pets" :[{"name": "Boots", "type": "dog", "age": "9", "unit": "years"}]
 }
@@ -8,8 +8,8 @@ let ourObject = {
 console.log(ourObject);
 
 let myData = {};
-
-fetch('https://catfact.ninja/fact')
+function fetchData() {
+    fetch('https://catfact.ninja/fact')
     .then(res =>{
         if(res.ok){
             return res.json();
@@ -19,12 +19,20 @@ fetch('https://catfact.ninja/fact')
         }).then(res => {
             myData = res;
             console.log(myData);
+            document.getElementById(`fact`).innerHTML = myData.fact;
         })
      .catch(error =>{console.log(error)})
 
+
+}
 fetchData();
 
-document.getElementById("generate").addEventListener()
+
+document.getElementById("generate").addEventListener("click", e=> {fetchData();});
 
 console.log(myData);
+
+let name = "Ava";
+// let myString = "hello" + name+ "!";
+let myString = `hello ${name} !`; 
 
